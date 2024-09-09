1e0d159f4d2dcf679fa51fedab6fcdc5de1b8355
KayjiYu <62629812+KayjiYu@users.noreply.github.com>
2024-09-09 17:11:26 -0400

    Update

--- Black_Friday_Sales_Prediction_Analysis_Regression/code.py
+++ Black_Friday_Sales_Prediction_Analysis_Regression/code.py
@@ -79,17 +79,66 @@ sns.heatmap(corr, annot=True, cmap='coolwarm')
 ## ----------Input Split------------------
 df.head()
 
-X = df.drop(['User_ID', 'Product_ID', 'Purchase'])
+X = df.drop(['User_ID', 'Product_ID', 'Purchase'], axis=1)
 y = df['Purchase']
 
 
 
+from sklearn.metrics import mean_squared_error
+## Model Training
+from sklearn.model_selection import cross_val_score, train_test_split
 
 
+def train(model, X, y):
+    # train-test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.25)
+    model.fit(X_train, y_train)
 
+    # predict the results
+    pred = model.predict(X_test)
 
+    # cross validation
+    cv_score = cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=5)
+    cv_score = np.abs(np.mean(cv_score))
+    
+    print("Results")
+    print("MSE:", np.sqrt(mean_squared_error(y_test, pred)))
+    print("CV Score:", np.sqrt(cv_score))
 
+# LinearRegression
+from sklearn.linear_model import LinearRegression
 
+model = LinearRegression()
+train(model, X, y)
+coef = pd.Series(model.coef_, X.columns).sort_values()
+coef.plot(kind='bar', title='Model Coefficients')
 
-# type: ignore # f2.canvas.manager.window.wm_geometry('+2000+1000')
+# DecisionTree
+from sklearn.tree import DecisionTreeRegressor
 
+model = DecisionTreeRegressor()
+train(model, X, y)
+features = pd.Series(model.feature_importances_, X.columns).sort_values(ascending=False)
+features.plot(kind='bar', title='Feature Importance')
+
+# RandomForest
+from sklearn.ensemble import RandomForestRegressor
+
+model = RandomForestRegressor(n_jobs=-1)
+train(model, X, y)
+features = pd.Series(model.feature_importances_, X.columns).sort_values(ascending=False)
+features.plot(kind='bar', title='Feature Importance')
+
+# Extra Tree
+import time
+
+start_time = time.time()
+
+from sklearn.ensemble import ExtraTreesRegressor
+
+model = ExtraTreesRegressor(n_jobs=-1)
+train(model, X, y)
+features = pd.Series(model.feature_importances_, X.columns).sort_values(ascending=False)
+features.plot(kind='bar', title='Feature Importance')
+print("-------------------------------")
+print(f"Execution Time: {(time.time() - start_time):.2f} seconds")
