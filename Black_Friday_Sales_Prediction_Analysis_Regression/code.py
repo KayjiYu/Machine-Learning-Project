5d5f4da6506540f782c270293b98a4f3a446409d
KayjiYu <62629812+KayjiYu@users.noreply.github.com>
2024-09-09 17:10:12 -0400

    Update

--- Black_Friday_Sales_Prediction_Analysis_Regression/code.py
+++ Black_Friday_Sales_Prediction_Analysis_Regression/code.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-%matplotlib inline
+%matplotlib 
 import matplotlib.pyplot as plt
 
 warnings.filterwarnings("ignore")
@@ -56,6 +56,33 @@ df.isnull().sum()
 gender_dict = {'F':0, 'M':1}
 df['Gender'] = df['Gender'].apply(lambda x: gender_dict[x])
 df.head()
+
+# to improve the metric use one hot encoding
+# label encoding
+cols = ['Age', 'City_Category', 'Stay_In_Current_City_Years']
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+for col in cols:
+    df[col] = le.fit_transform(df[col])
+df.head()
+
+
+
+## ----------Input Split------------------
+corr = df.drop(['User_ID', 'Product_ID'], axis=1).corr()
+plt.figure(figsize=(14, 7))
+sns.heatmap(corr, annot=True, cmap='coolwarm')
+
+
+
+## ----------Input Split------------------
+df.head()
+
+X = df.drop(['User_ID', 'Product_ID', 'Purchase'])
+y = df['Purchase']
+
+
 
 
 
