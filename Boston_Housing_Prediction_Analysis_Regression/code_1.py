b7b1387cd015d7fdcc8b81d166e9146e1fbe7b78
KayjiYu <62629812+KayjiYu@users.noreply.github.com>
2024-09-09 21:19:25 -0400

    Update

--- Boston_Housing_Prediction_Analysis_Regression/code_1.py
+++ Boston_Housing_Prediction_Analysis_Regression/code_1.py
@@ -1,9 +1,10 @@
 ## ------------Import modules--------------
-import pandas as pd
-import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
-%matplotlib inline
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+%matplotlib
 
 df = pd.read_csv("Boston_Dataset.csv")
 df.head()
@@ -21,3 +22,96 @@ df.info
 
 # check for null values
 df.isnull().sum()
+
+
+## ------------Exploratory Data Analysis-----------
+
+# create box plots
+fig, ax = plt.subplots(ncols=7, nrows=2, figsize=(20, 10))
+index = 0
+ax = ax.flatten()
+
+for col, value in df.items():
+    sns.boxplot(y=col, data=df, ax=ax[index])
+    index += 1
+
+plt.tight_layout(pad=0.5, w_pad=0.7, h_pad=5.0)
+
+# create dis plot
+fig, ax = plt.subplots(ncols=7, nrows=2, figsize=(20,10))
+index = 0
+ax = ax.flatten()
+
+for col, value in df.items():
+    sns.displot(data=value, ax=ax[index])
+    index += 1
+
+plt.tight_layout(pad=0.5, w_pad=0.7, h_pad=5.0)
+
+
+
+## ---------------Coorelation Matrix--------------
+corr = df.corr()
+plt.figure(figsize=(20,10))
+sns.heatmap(corr, annot=True, cmap='coolwarm')
+
+sns.regplot(y=df['medv'], x=df['lstat'])
+
+sns.regplot(y=df['medv'], x=df['rm'])
+
+
+
+## --------------Input split-------------------
+X = df.drop(columns=['medv', 'rad'], axis=1)
+y=df['medv']
+
+
+
+## -------------Model Training-----------------
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.metrics import mean_squared_error
+
+def train(model, X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+    model.fit(X_train, y_train)
+    pred = model.predict(X_test)
+    cv_score = cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=5)
+    cv_score = np.abs(np.mean(cv_score))
+    print("Model Report")
+    print("MSE:", mean_squared_error(y_test, pred))
+    print('CV Score:', cv_score)
+
+# LinearRegression
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+train(model, X, y)
+coef = pd.Series(model.coef_, X.columns).sort_values()
+coef.plot(kind='bar', title='Model Coefficients')
+
+# DecisionTree
+from sklearn.tree import DecisionTreeRegressor
+model = DecisionTreeRegressor()
+train(model, X, y)
+coef = pd.Series(model.feature_importances_, X.columns).sort_values(ascending=False)
+coef.plot(kind='bar', title='Feature Importance')
+
+# RandomForest
+from sklearn.ensemble import RandomForestRegressor
+model = RandomForestRegressor()
+train(model, X, y)
+coef = pd.Series(model.feature_importances_, X.columns).sort_values(ascending=False)
+coef.plot(kind='bar', title='Features Importance')
+
+# ExtraTrees
+from sklearn.ensemble import ExtraTreesRegressor
+model = ExtraTreesRegressor()
+train(model, X, y)
+coef = pd.Series(model.feature_importances_, X.columns).sort_values(ascending=False)
+coef.plot(kind='bar', title='Feature Importance')
+
+# Xgboost
+from xgboost import XGBRegressor
+model = XGBRegressor()
+train(model, X, y)
+coef = pd.Series(model.feature_importances_, X.columns).sort_values(ascending=False)
+coef.plot(kind='bar', title='Feature Importance')
